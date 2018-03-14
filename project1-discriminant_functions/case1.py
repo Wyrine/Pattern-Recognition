a0b88db881968a9e90cee6ca75c5469903091420
Wyrine <kashahat@gmail.com>
2018-03-13 19:14:34 -0500

    added generalized roc python script

--- project1-discriminant_functions/case1.py
+++ project1-discriminant_functions/case1.py
@@ -45,20 +45,21 @@ def generateFunc( eq ):
 
 
 if __name__ == "__main__":
-    xTr, yTr, xTr1, yTr1, xTe, yTe, xTe1, yTe1 = getData("./data/synth.tr", "./data/synth.te")
-    train0, = plt.plot(xTr, yTr, 'r.', label="Training Data, Class 0")
-    train1, = plt.plot(xTr1, yTr1, 'g.', label="Training Data, Class 1")
-    test0, = plt.plot(xTe, yTe, 'c.', label="Testing Data, Class 0")
-    test1, = plt.plot(xTe1, yTe1, 'y.', label="Testing Data, Class 1")
-    test = np.concatenate((xTe, xTe1))
-    m, b = -0.832623, 0.443782
+		xTr, yTr, xTr1, yTr1, xTe, yTe, xTe1, yTe1 = getData("./data/synth.tr", "./data/synth.te")
+		train0, = plt.plot(xTr, yTr, 'r.', label="Training Data, Class 0")
+		train1, = plt.plot(xTr1, yTr1, 'g.', label="Training Data, Class 1")
+		test0, = plt.plot(xTe, yTe, 'c.', label="Testing Data, Class 0")
+		test1, = plt.plot(xTe1, yTe1, 'y.', label="Testing Data, Class 1")
+		test = np.concatenate((xTe, xTe1))
+		m, b = -0.832623, 0.443782
     #need to write a function that generates a ton of points 
     #x and y based off of a function so that I can use it for the line and such
-    case1Line, = plt.plot(test, m * test + b, '-', label= "Decision Region for Case 1.")
-    plt.legend(handles = [train0, train1, test0, test1, case1Line])
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.title("Decision Regions for Case 1")
-    plt.grid()
-    plt.text(-0.8, 1.2, "y = -0.832623x + 0.443782", color="black")
-    plt.show()
+		case1Line, = plt.plot(test, m * test + b, '-', label= "Decision Region for Case 1.")
+		plt.legend()
+		#plt.legend(handles = [train0, train1, test0, test1, case1Line])
+		plt.xlabel('x')
+		plt.ylabel('y')
+		plt.title("Decision Regions for Case 1")
+		plt.grid()
+		plt.text(-0.8, 1.2, "y = -0.832623x + 0.443782", color="black")
+		plt.show()
