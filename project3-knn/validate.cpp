970de995820f9471ec8c356efa6af72dea2592de
Wyrine <kashahat@gmail.com>
2018-03-17 16:28:01 -0400

    properly reading the grouping file

--- project3-knn/validate.cpp
+++ project3-knn/validate.cpp
@@ -1,11 +1,13 @@
 #include "validate.hpp"
+#include <cctype>
 
 using namespace std;
 
 Validation::Validation(const char* data, const char* grp)
 {
-
-
+		thread t1(&Validation::readGroupingFile, this, grp);
+		//thread t2(&Validation::readAndBuildMatrix, this, data);
+		t1.join(); //t2.join();
 }
 		void
 Validation::Validate(const uint m)
@@ -14,13 +16,29 @@ Validation::Validate(const uint m)
 
 }
 		void
-Validation::readGroupingFile(const char*)
+Validation::readGroupingFile(const char* grp)
 {
-
-
+		ifstream input(grp);
+		if(! input.is_open() )
+		{
+				cerr << "Failed to open " << grp << ". Exiting.";
+				exit(1);
+		}
+		char junk;
+		vector<short> curLine;
+		short curVal;
+		do{
+				do{
+						input >> curVal;
+						curLine.push_back(curVal);
+				}while( isspace(input.peek()) );
+				groups.push_back(curLine);
+				curLine.clear();
+		}while( input >> junk );
+		input.close();
 }
 		void
-Validation::readAndBuildMatrix(const char*)
+Validation::readAndBuildMatrix(const char* data)
 {
 
 
